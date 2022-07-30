2854ad1b7d81e0b53d5a728a682b57d6371e7a60
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2022-07-30 15:14:05 +0200

    Oupsi j'ai tout casser avec un conflits

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,18 +9,42 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 10f;
 
-    Rigidbody2D rb;
+    public float limitY = 4f;
+    public float limitX = 6f;
+
+    private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-  
+
     void Update()
     {
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
 
         rb.velocity = new Vector2(horizontalInput, verticalInput) * speed;
+
+        if (transform.position.y < -limitY && verticalInput < 0)
+            rb.velocity = rb.velocity * Vector2.right;
+
+        if (transform.position.y > limitY && verticalInput > 0)
+            rb.velocity = rb.velocity * Vector2.right;
+
+        if (transform.position.x < -limitX && horizontalInput < 0)
+            rb.velocity = rb.velocity * Vector2.up;
+
+        if (transform.position.x > limitX && horizontalInput > 0)
+            rb.velocity = rb.velocity * Vector2.up;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(new Vector3(limitX, 0, 0), Vector3.one);
+        Gizmos.DrawWireCube(new Vector3(-limitX, 0, 0), Vector3.one);
+
+        Gizmos.DrawWireCube(new Vector3(0, limitY, 0), Vector3.one);
+        Gizmos.DrawWireCube(new Vector3(0, -limitY, 0), Vector3.one);
     }
 }
