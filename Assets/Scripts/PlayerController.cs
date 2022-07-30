d82429259991be321c9d2bc87c157769168f1956
Yuna <yuna.bonnifet@gmail.com>
2022-07-30 15:04:27 +0200

    New Limits

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,18 +9,51 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 10f;
 
+    public float limitY = 4f;
+    public float limitX = 6f;
+
+    
+
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
   
     void Update()
     {
-        verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector2.up * verticalInput * Time.deltaTime * speed);
 
+        verticalInput = Input.GetAxis("Vertical");       
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector2.right * horizontalInput * Time.deltaTime * speed);
 
+        rb.velocity = new Vector2(horizontalInput, verticalInput) * speed ;
+
+        if (transform.position.y < -limitY && verticalInput < 0)
+        {
+            rb.velocity = rb.velocity * Vector2.right;
+        }
+        if (transform.position.y > limitY && verticalInput > 0)
+        {
+            rb.velocity = rb.velocity * Vector2.right;
+        }
+        if (transform.position.x < -limitX && horizontalInput < 0)
+        {
+            rb.velocity = rb.velocity * Vector2.up;
+        }
+        if (transform.position.x > limitX && horizontalInput > 0)
+        {
+            rb.velocity = rb.velocity * Vector2.up;
+        }
+
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(new Vector3(limitX, 0, 0), Vector3.one);
+        Gizmos.DrawWireCube(new Vector3(-limitX, 0, 0), Vector3.one);
+
+        Gizmos.DrawWireCube(new Vector3(0, limitY, 0), Vector3.one);
+        Gizmos.DrawWireCube(new Vector3(0, -limitY, 0), Vector3.one);
     }
 }
